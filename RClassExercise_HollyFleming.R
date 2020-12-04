7134fcd86d37efd9aa3fb6bcd4985647d366eb5d
Holly Fleming <hfleming001@dundee.ac.uk>
2020-12-04 17:00:09 +0000

    Q4 complete

--- RClassExercise_HollyFleming.R
+++ RClassExercise_HollyFleming.R
@@ -44,9 +44,69 @@ numREdit <- c(numREdit, 12:16); numREdit; length(numREdit) #length = 30
 numREdit[c(9,11,13)]
 
 
+
+
 ## 3 Creating a vector called Evi containing blood spatter BS, hair H, glass fragments GF, paint P, gunshot residue GSR and fibre F.
 
 Evi <- c("BS", "H", "GF", "P", "GSR", "F"); Evi
 
 # a)
+Evi2 <- matrix(Evi, nrow = 2, ncol = 3); Evi2
+
+# b)
+Evi2[,2]
+
+# c)
+Evi3 <- matrix(Evi, nrow = 3, ncol = 2); Evi3
+
+# d)
+Evi3[2,]
+
+# e)
+Evi3R <- matrix(Evi, nrow = 3, ncol = 2, byrow = TRUE); Evi3R
+
+# f)
+Evi3 == Evi3R # no
+identical(Evi3, Evi3R) # False
+
+# g)
+rbind(Evi3,Evi3R)
+
+# h)
+cbind(Evi3,Evi3R)
+
+
+
+## 4 Creating vectors for a series of refractive indices. 
+
+RIsu <- c(2.8,3.1,1.9,2.4,2.1); RIsu
+RIcs <- c(2.1,1.5,3.2,2.4,2.9); RIcs
+
+
+# a)
+rbind(RIsu,RIcs)
+
+# b)
+cbind(RIsu,RIcs)
+
+# c)
+measRI <- data.frame(cbind(RIsu,RIcs)); measRI
+
+# d)
+ID <- (1:5); ID
+#measRI[3] <- ID; measRI
+#or
+mRIID <- measRI
+mRIID$ID <- c(1:5); mRIID
+
+# e)
+# mRIID[3,] <- c(2.35, 2.45,3); mRIID    # not sure how to change the first 2 without the 3rd column in a single line?
+# or
+mRIID[3,1] <- c(2.35); mRIID
+mRIID[3,2] <- c(2.45); mRIID
+
+
+
+## 5 Joint matrices
+
 
